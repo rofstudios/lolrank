ba1fd797afff060276a33034b251ee2fa35957d4
Your Name <you@example.com>
2022-11-28 00:48:00 -0500

    created web design and coded it 90% done, footer incomplete

--- lolrank/components/footer.js
+++ lolrank/components/footer.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+import footerImg from "../public/footer.png"
+// css
+import styles from "../styles/Footer.module.css"
 export default function Footer() {
     return (
-        <h3>LoL Rank & Yachi&apos; Corner 2022 - 2023</h3>
+        <div className={styles.footerContainer} style={{zIndex: 1, color: "black" }}>
+            <Image
+                alt="footer background image"
+                src={footerImg}
+                fill={true}
+                // object-fit={"cover"}
+                style={{ zIndex: -1, position: "absolute" }}
+            />
+            <h3>LoL Rank & Yachi&apos; Corner 2022 - 2023</h3>
+        </div>
     )
 }
